2b3e48431e7e4771cf98cedf8c2d49896cd489ae
Danny Kruitbosch <danny.kruitbosch@spronq.com>
2017-11-13 08:10:51 +0100

    POST point naar map layer toegevoegd

---
+++ models/geojson.js
@@ -0,0 +1,19 @@
+var GeoJSON = require('mongoose-geojson-schema');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+ 
+var schema = new Schema({
+    _id: {type: Schema.ObjectId, auto: true},
+    point: mongoose.Schema.Types.Point,
+    multipoint: mongoose.Schema.Types.MultiPoint,
+    linestring: mongoose.Schema.Types.LineString,
+    multilinestring: mongoose.Schema.Types.MultiLineString,
+    polygon: mongoose.Schema.Types.Polygon,
+    multipolygon: mongoose.Schema.Types.MultiPolygon,
+    geometry: mongoose.Schema.Types.Geometry,
+    geometrycollection: mongoose.Schema.Types.GeometryCollection,
+    feature: mongoose.Schema.Types.Feature,
+    featurecollection: mongoose.Schema.Types.FeatureCollection
+}, {collection: 'layercollection'});
+
+module.exports = mongoose.model('GeoJSON', schema);
