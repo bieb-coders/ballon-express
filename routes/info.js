f7df39db457f1c92db6b33e3824241913de9336f
JarnoEetSoep <romijnjarno@gmail.com>
2018-03-03 15:28:25 +0100

    infopage = NOG NIET AF = (doet het niet)

--- routes/info.js
+++ routes/info.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  //res.render('index', { title: 'CoderDojo Ballon Express' });
-  res.redirect('/dashboard');
+  res.render('info', { title: 'Informatie' });
 });
 
 module.exports = router;
